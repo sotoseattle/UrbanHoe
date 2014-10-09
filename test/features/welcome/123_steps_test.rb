306f76f35ddf531824d00b22cf91b070870a0684
Javier Soto <sotoseattle@gmail.com>
2014-10-09 13:37:07 -0700

    tests for online presence and zip call

--- test/features/welcome/123_steps_test.rb
+++ test/features/welcome/123_steps_test.rb
@@ -1,16 +1,25 @@
 require 'test_helper'
 
 feature 'Zipcode functionality' do
-  # scenario 'happy path' do
-  #   visit root_path
-  #   fill_in 'zipo', with: 98102
-  #   click_on 'mysubmit1'
-  #   page.text.must_include 'message'
-  #   page.find('#hhregion').text.must_equal ''
-  # end
+  scenario 'happy path' do
+    visit root_path
+    fill_in 'zipo', with: '98102'
+    click_on 'mysubmit1'
+    h = find('#hhregion', :visible => false)
+    sleep 3
+    h.value.must_equal '8'
+  end
+
+  scenario 'invalid query' do
+    visit root_path
+    fill_in 'zipo', with: 'hola'
+    click_on 'mysubmit1'
+    h = find('#hhregion', :visible => false)
+    h.value.must_equal '0'
+  end
 end
 
-feature 'Zipcode functionality' do
+feature '' do
   scenario 'happy path' do
 
   end
