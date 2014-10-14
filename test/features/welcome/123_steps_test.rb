36ec1df16ca72bd1d4831efbc2f0fe40836b611d
Javier Soto <sotoseattle@gmail.com>
2014-10-13 18:46:09 -0700

    remove all online tests

--- test/features/welcome/123_steps_test.rb
+++ test/features/welcome/123_steps_test.rb
@@ -2,25 +2,19 @@ require 'test_helper'
 
 feature 'Zipcode functionality' do
   scenario 'happy path' do
-    visit root_path
-    fill_in 'zipo', with: '98102'
-    click_on 'mysubmit1'
-    h = find('#hhregion', visible: false)
-    sleep 3
-    h.value.must_equal '8'
+    # visit root_path
+    # fill_in 'zipo', with: '98102'
+    # click_on 'mysubmit1'
+    # h = find('#hhregion', visible: false)
+    # sleep 3
+    # h.value.must_equal '8'
   end
 
   scenario 'invalid query' do
-    visit root_path
-    fill_in 'zipo', with: 'hola'
-    click_on 'mysubmit1'
-    h = find('#hhregion', visible: false)
-    h.value.must_equal '0'
-  end
-end
-
-feature '' do
-  scenario 'happy path' do
-
+    # visit root_path
+    # fill_in 'zipo', with: 'hola'
+    # click_on 'mysubmit1'
+    # h = find('#hhregion', visible: false)
+    # h.value.must_equal '0'
   end
 end
