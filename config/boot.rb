b16b0defe0d316f33bdfb2bd32043fc567601f5f
Javier Soto <sotoseattle@gmail.com>
2014-11-24 13:43:25 -0800

    trace heroku error r10

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# added to trace heroku R-10 error
+require File.expand_path('../initializers/debug_require', __FILE__)
